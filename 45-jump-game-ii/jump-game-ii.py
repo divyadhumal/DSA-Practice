e3a412406665771b06a0f467c1d0d0659e550ba9
Divya Dhumal <dhumaldivya3@gmail.com>
2026-05-19 20:44:40 +0530

    Time: 3 ms (89.34%) | Memory: 20.1 MB (66.66%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,14 +1,26 @@
 class Solution:
-  def jump(self, nums: List[int]) -> int:
-    jumps = 0
-    current_end = 0
-    farthest = 0
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
 
-    for i in range(len(nums) - 1):
-        farthest = max(farthest, i + nums[i])
+        jumps = 0
+        curr_end = 0   # end of current jump range
+        max_reach = 0  # farthest index we can reach while scanning this range
 
-        if i == current_end:
-            jumps += 1
-            current_end = farthest
+        # We only need to process until n-2, because once we can reach last index,
+        # we stop. No need to jump from the last index itself.
+        for i in range(n - 1):
+            max_reach = max(max_reach, i + nums[i])
 
-    return jumps
+            # Time to take one jump: we finished scanning current range.
+            if i == curr_end:
+                jumps += 1
+                curr_end = max_reach
+
+                # Optimization: if this jump can reach/past last index, done.
+                if curr_end >= n - 1:
+                    break
+
+        return jumps
+        
